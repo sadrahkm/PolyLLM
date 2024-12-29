d8c00b4e12da0423aac615d6bf54991c5085ea3d
sadrahkm <sadrahakim2000@gmail.com>
2024-12-28 21:21:43 -0500

    Fixed directory paths

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
 import os
 
 BASE_PATH = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
-EMBEDDING_PATH = BASE_PATH + '/output/pairs'
+OUTPUT_PATH = BASE_PATH + '/output'
+EMBEDDING_PATH = OUTPUT_PATH + '/pairs'
 DATA_PATH = BASE_PATH + '/data/'
 MODELS_PATH = BASE_PATH + '/models/'
 LOG_PATH = BASE_PATH + '/logs/'
+CHECKPOINT_PATH = OUTPUT_PATH + '/checkpoints'
