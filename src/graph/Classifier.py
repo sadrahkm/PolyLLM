b3d4fc578e201bf005072f358c4de223368abdaf
sadrahkm <sadrahakim2000@gmail.com>
2025-01-10 00:04:34 -0500

    Included the ption to get edge embeddings

--- src/graph/Classifier.py
+++ src/graph/Classifier.py
@@ -4,10 +4,13 @@ from torch import Tensor
 # Our final classifier applies the dot-product between source and destination
 # node embeddings to derive edge-level predictions:
 class Classifier(torch.nn.Module):
-    def forward(self, x_pdrugs: Tensor, x_seffect: Tensor, edge_label_index: Tensor) -> Tensor:
+    def forward(self, x_pdrugs: Tensor, x_seffect: Tensor, edge_label_index: Tensor, integrate) -> Tensor:
         # Convert node embeddings to edge-level representations:
         edge_feat_pdrugs = x_pdrugs[edge_label_index[0]]
         edge_feat_seffect = x_seffect[edge_label_index[1]]
 
         # Apply dot-product to get a prediction per supervision edge:
-        return (edge_feat_pdrugs * edge_feat_seffect).sum(dim=-1)
+        if integrate:
+            return edge_feat_pdrugs * edge_feat_seffect
+        else:
+            return (edge_feat_pdrugs * edge_feat_seffect).sum(dim=-1)
